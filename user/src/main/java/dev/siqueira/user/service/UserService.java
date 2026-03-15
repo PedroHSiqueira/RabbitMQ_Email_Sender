f4a1882f79cb5da6cec4f5908d49153b0836ad18
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2026-03-15 15:06:53 -0300

    Refactor: Adição do disparo Rabbit no salvamento (Dentro de uma transactional)

--- user/src/main/java/dev/siqueira/user/service/UserService.java
+++ user/src/main/java/dev/siqueira/user/service/UserService.java
@@ -1,6 +1,7 @@
 package dev.siqueira.user.service;
 
 import dev.siqueira.user.entity.User;
+import dev.siqueira.user.producer.UserProducer;
 import dev.siqueira.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final UserProducer userProducer;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserProducer userProducer, UserRepository userRepository) {
+        this.userProducer = userProducer;
         this.userRepository = userRepository;
     }
 
     @Transactional
     public User save(User user) {
-        return userRepository.save(user);
+        User userSaved = userRepository.save(user);
+        userProducer.sendEventAfterCreate(userSaved);
+        return userSaved;
     }
 }
